d1df50eff47dd0e5cc00f084a8075508a5a1d3c4
quickselect <quickselect@proton.me>
2023-10-26 21:56:03 +0300

    fixed methods

--- CircularDoublyLinkedList.java
+++ CircularDoublyLinkedList.java
@@ -1,40 +1,69 @@
 import Node.Node;
+import java.util.NoSuchElementException;
 
 class CircularDoublyLinkedList<E>{
   int size = 0;
   // pointer to first node.
   Node<E> first;
-  // pointer to last node.
-  Node<E> last;
 
   public CircularDoublyLinkedList(){}
 
   void deleteList(){
     first = null;
-    last = null;
   }
 
   void push(E new_data){
     Node<E> new_node = new Node(new_data);
-
-    new_node.next = start;
+    if(first==null){
+      new_node.next = new_node.prev = new_node;
+      first = new_node;
+      return;
+    }
+    Node<E> last = first.prev;
+    new_node.next = first;
     new_node.prev = last;
-
     last.next = first.prev = new_node;
-
     first = new_node;
+    ++size;
   }
 
-  // basically the same thing as the push method
-  void append(E new_data){
-    Node<E> new_node = new Node(new_data);
+  // T(N) S(1)
+  void remove(Node<E> e){
+    // list is empty
+    if(first == null) throw new NoSuchElementException();
 
-    new_node.next = start;
-    new_node.prev = last;
+    // find the node.
+    Node curr = first, prev_1 = null;
+    while(e != curr){
+      if(curr.next == first){
+        throw new NoSuchElementException();
+      }
+      prev_1 = curr;
+      curr = curr.next;
+    }
 
-    last.next=first.prev=new_node;
+    if(size == 1){
+      first = null;
+      --size;
+      return;
+    }
 
-    last = new_node;
+    // is it the first node?
+    if(curr == first) {
+      prev_1 = first.prev;
+      first = first.next;
+      prev_1.next = first;
+      first.prev = prev_1;
+    } else if (curr.next == first) { // is it the last node?
+      prev_1.next = first;
+      first.prev = prev_1;
+    } else {
+      Node<E> temp = curr.next;
+      prev_1.next = temp;
+      temp.prev = prev_1;
+    }
+    --size;
+    return;
   }
 
   void InsertAfter(Node<E> prev_node, E new_data) {
@@ -65,7 +94,7 @@ class CircularDoublyLinkedList<E>{
   public String toString(){
     StringBuilder str = new StringBuilder();
     str.append("[ ");
-    Node<E> temp = head;
+    Node<E> temp = first;
     if(temp==null){
       str.append("]");
       return str.toString();
@@ -73,18 +102,20 @@ class CircularDoublyLinkedList<E>{
     do {
       str.append(temp.data.toString() + " -> ");
       temp = temp.next;
-    } while(temp!=first)
+    } while(temp!=first);
     str.append("NULL ]");
     return str.toString();
   }
 
-  static void main(String[] args) {
+  public static void main(String[] args) {
     CircularDoublyLinkedList<Integer> cdll = new CircularDoublyLinkedList<>();
     cdll.push(1);
     cdll.push(2);
     cdll.push(3);
     cdll.push(4);
-    System.out.printl(cdll.toString());
+    System.out.println(cdll.toString());
+    cdll.remove(cdll.first.next.next);
+    System.out.println(cdll.toString());
 //    cdll.reverseIteratively()
 //    System.out.printl(cdll.toString());
   }
