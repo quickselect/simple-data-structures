c9e076842266be5bcc1a735aaf907e98739641b5
quickselect <quickselect@proton.me>
2023-12-17 18:44:25 +0300

    erase method is not working

--- Vector.java
+++ Vector.java
@@ -1,3 +1,5 @@
+import Node.Node;
+
 import java.lang.reflect.Array;
 import java.util.NoSuchElementException;
 import java.lang.IndexOutOfBoundsException;
@@ -5,12 +7,12 @@ import java.lang.IndexOutOfBoundsException;
 public class Vector<E> {
   E[] container = null;
   int currSize = 0;
-  int lastElemIndex = 0;
+  int containerSize = 0;
 
   Vector(int INITIAL_ARRAY_LENGTH){
-    //this.container = new Object[size];
     E[] arr = (E[]) new Object[INITIAL_ARRAY_LENGTH];
     this.container = arr;
+    this.containerSize = INITIAL_ARRAY_LENGTH;
   }
 
   private void checkBounds(int pos){
@@ -57,7 +59,8 @@ public class Vector<E> {
   }
 
   void clear(){
-    this.container = null;
+    E[] arr = (E[]) new Object[0];
+    this.container = arr;
     this.currSize = 0;
   }
 
@@ -69,15 +72,12 @@ public class Vector<E> {
 
     ++currSize;
 
-    if(pos == 0) {
-      this.push_back(value);
-      return;
-    }
-
     // if it is not the case that current container size is enough.
     if(!isContainerCapacityEnough()) {
+      // increase the capacity by 1.5 * N. N being the current container size.
+      int newSize = (int) Math.ceil(currSize * 1.5);
       // currSize plus one because we are adding one more element.
-      E[] newContainer = (E[]) new Object[currSize+1];
+      E[] newContainer = (E[]) new Object[newSize];
 
       // before pos
       for(int i=0;i<=pos;++i){
@@ -89,6 +89,8 @@ public class Vector<E> {
         newContainer[i] = this.container[i-1];
       }
       this.container = newContainer;
+      this.containerSize = newSize;
+      ++this.currSize;
       return;
     } else { // using sliding window technique here
       int left = currSize-1;
@@ -108,8 +110,7 @@ public class Vector<E> {
 
     --currSize;
 
-    if(pos == lastElemIndex){
-      lastElemIndex -= 1;
+    if(pos == this.currSize){
       this.pop_back();
       return;
     } else {
@@ -121,11 +122,81 @@ public class Vector<E> {
     }
   }
 
-  /*void push_back(E value) {
-    if(!isContainerCapacityEnough()) {
+  // complexity: constant
+  // exceptions: throws nothing
+  void pop_back(){
+    this.container[currSize-1] = null;
+    this.currSize -= 1;
+  }
 
+  void push_back(E value){
+    if(isContainerCapacityEnough()) {
+      this.container[currSize] = value;
+      ++this.currSize;
+    } else {
+      int newSize = (int) Math.ceil(currSize * 1.5);
+      // currSize plus one because we are adding one more element.
+      E[] newContainer = (E[]) new Object[newSize];
+      for(int i=0;i<currSize;++i){
+        // copy everything to new container
+        newContainer[i]=this.container[i];
+      }
+      newContainer[currSize] = value;
+      ++this.currSize;
+      this.container = newContainer;
+      this.containerSize = newSize;
     }
-  }*/
+  }
+
+  public String toString(){
+    StringBuilder str = new StringBuilder();
+    str.append("[ ");
+    for(int i=0;i<currSize;++i){
+      if(i!=0)
+        str.append(", ");
+      str.append(this.container[i]);
+    }
+    str.append(" ]");
+    return str.toString();
+  }
+  public static void main(String[] args){
+    // at, front, back, data, empty, size, capacity, clear, insert, erase, pop_back
+    Vector<Integer> test = new Vector<>(10);
+    test.push_back(3);
+    test.push_back(4);
+    test.push_back(5);
+    test.push_back(6);
+    System.out.println(test.toString());
+
+    System.out.println(test.at(0));
+    System.out.println(test.front());
+    System.out.println(test.back());
+
+    System.out.println(test.empty());
+    System.out.println(test.size());
+    System.out.println(test.capacity());
+
+    test.insert(0,1);
+    System.out.println(test.toString());
+    System.out.println(test.size());
+    System.out.println(test.capacity());
+
+    test.pop_back();
+    System.out.println(test.toString());
+    System.out.println(test.size());
+    System.out.println(test.capacity());
+
+    test.push_back(55);
+    System.out.println(test.toString());
+    System.out.println(test.size());
+    System.out.println(test.capacity());
+
+    // ERROR
+    test.erase(0);
+    System.out.println(test.toString());
+    System.out.println(test.size());
+    System.out.println(test.capacity());
+  }
 }
 
 class out_of_range extends RuntimeException {
